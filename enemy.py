e1ec22e33995812a70809ede63d4fc980191ada2
alexkritikov <kritikovalex@gmail.com>
2023-11-04 14:31:42 +0300

    Enemy

--- enemy.py
+++ enemy.py
@@ -1,17 +1,19 @@
 from .character import Character
 import random
 
-
-    def attack(self):
-       pass
-    def defence(self):
-       pass
 class Enemy(Character):
     def atack(self):
         pass
     def defence(self):
         pass
-enemy_act = random.randint(1,2)
+    def choose_action(self):
+        enemy_act = random.randint(1,2)
+        if enemy_act == 1:
+            return 'Attack'
+        else:
+            return 'Defence'
+
+
 class Zombi(Enemy):
     defence_koef = 1
     attack_koef = 4
@@ -19,8 +21,4 @@ class Skelet(Enemy):
     defence_koef = 2
     attack_koef = 3
 
-def enemy_action():
-    if enemy_act==1:
-        Enemy.atack()
-    else:
-        Enemy.defence()
+
