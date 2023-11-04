66c1339f4f9dfa3c8e6bfef8679aca842b04ccbb
alexkritikov <kritikovalex@gmail.com>
2023-11-04 14:10:44 +0300

    Enemy

--- enemy.py
+++ enemy.py
@@ -1,19 +1,6 @@
+from .character import Character
 import random
-import math
-class Character:
-    initial_health = 100
-    initial_defence = 10
-    initial_attack = 10
 
-    health_koef = 1
-    defence_koef = 1
-    attack_koef = 1
-
-    def __init__(self, name):
-        self.name = name
-        self.health_value = self.initial_health * self.health_koef
-        self.defence_value = self.initial_defence * self.initial_defence
-        self.attack_value = self.initial_attack * self.initial_attack
 
     def attack(self):
        pass
